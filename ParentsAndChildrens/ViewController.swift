1fd3a48d050823822fe81f4b759013cbe9dbba3e
Timofey Kharitonov <dev.kharitonov@yandex.ru>
2022-02-28 22:59:56 +0300

    Initial Commit

---
+++ ParentsAndChildrens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ParentsAndChildrens
+//
+//  Created by Timofey Kharitonov on 28.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
